f14558027d08c10fa803fb410e5a4d1b71830e5f
chae <89851362+BIONIChae@users.noreply.github.com>
2026-03-15 10:30:52 +0200

    Bank Account Details

---
+++ BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBankingApp
+{
+    internal class BankAccount
+    {
+        public string Owner { get; set; }
+        public Guid AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
+ 
